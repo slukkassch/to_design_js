1819bf24ec15bc1f76e76a171ab9d0bd8f5557aa
Luccas schmidt <150750093+slukkassch@users.noreply.github.com>
2024-05-12 18:07:52 -0300

    Adicionando arquivos

---
+++ sketch.js
@@ -0,0 +1,72 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  }
+  
+  function draw() {
+      background("bleck");
+      
+      rect(0, 10, 100, 150);
+  }
+  
+  function draw() {
+      background("bleck");
+      
+      rect(0, 10, 100, 150);
+  }
+  
+  function draw() {
+      background("bleck");
+  
+      fill("red");
+      rect(0, 10, 100, 150);
+  }
+  
+  function draw() {
+      background("bleck");
+      
+      stroke("blue") 
+      fill("red") 
+      rect(0, 10, 100, 150)
+  }
+  
+  function draw() {
+    background(220);
+    
+    stroke("blue");
+    fill("red");
+    rect(mouseX, mouseY, 100, 150);
+  }
+  
+  function draw() {
+    background(0);
+    
+    stroke("blue");
+    fill("red");
+    
+    console.log(mouseIsPressed);
+    rect(mouseX, mouseY, 100, 150);
+  }
+  
+  function setup() {
+    // cria uma tela de 400px de largura por 400px de altura
+    createCanvas(400, 400);
+  }
+  
+  function setup() {
+    createCanvas(400, 400);
+    background("#white")
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("red");
+    
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
